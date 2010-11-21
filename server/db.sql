d08e19deb449d06ef84ff5a486f18535025d7162
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2010-11-21 14:23:24 +0100

    Se incluye la inicializacion de la sesion y un nuevo archivo de configuracion

---
+++ server/db.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS sessions CASCADE;
+DROP TABLE IF EXISTS usr CASCADE;
+DROP TABLE IF EXISTS video CASCADE;
+
+CREATE TABLE sessions (
+    session_id char(128) UNIQUE NOT NULL,
+    atime timestamp NOT NULL default current_timestamp,
+    data text
+);
+
+CREATE TABLE usr (
+       id serial NOT NULL PRIMARY KEY,
+       username VARCHAR(50) NOT NULL UNIQUE,
+       password VARCHAR(50) NOT NULL,
+       name VARCHAR(50),
+       surname VARCHAR(50)
+);
+
+CREATE TABLE video(
+       id serial NOT NULL PRIMARY KEY,
+       name VARCHAR(80) NOT NULL,
+       format VARCHAR(20) NOT NULL,
+       id_owner integer NOT NULL REFERENCES usr(id)
+);
--- server/rstr_web.py
+++ server/rstr_web.py
@@ -1,23 +1,41 @@
 #!/usr/bin/env python
 
+import os
+import sys
 import web
 from web import form
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import rstr_config
+
+web.config.debug = True
 # /(.*)
 urls = (
   '/', 'index',
   '/about', 'about'
 )
 
-web.config.debug = True
 app = web.application(urls, globals())
 render = web.template.render('/home/i02sopop/desarrollo/rstreaming/server/templates/')
+
+if web.config.debug:
+    if web.config.get('_session') is None:
+        db = web.database(dbn='postgres', db='rstreaming', user='i02sopop', pw='')
+        store = web.session.DBStore(db, 'sessions')
+        session = web.session.Session(app, store, {})
+        web.config._session = session
+    else:
+        session = web.config._session
+else:
+    db = web.database(dbn='postgres', db='rstreaming', user='i02sopop', pw='')
+    store = web.session.DBStore(db, 'sessions')
+    session = web.session.Session(app, store, initializer={})
 
 login = form.Form(form.Textbox('username'),
                   form.Password('password'),
                   form.Password('password_again'),
-                  validators = [form.Validator("Las contrasenas no coinciden.", lambda i: i.password == i.password_again)]
-                  )
+                  validators = [form.Validator("Las contrasenas no coinciden.", lambda i: i.password == i.password_again)])
 
 class index:
     def GET(self):
@@ -42,7 +60,8 @@ class index:
 class about:
     def GET(self):
         form = login()
-        left = render.left(form)
+        blocks = [render.login(form), render.section(render.article("Title","abstract","content"))]
+        left = render.left(blocks)
         center = render.center()
         right = render.right()
         return render.index(unicode(left), unicode(center), unicode(right), "Ritho's streaming", "static/style.css", "static/jquery.js", "")
@@ -57,6 +76,8 @@ class about:
         center = render.center()
         right = render.right()
         return render.index(unicode(left), unicode(center), unicode(right), "Ritho's streaming", "static/style.css", "static/jquery.js", "")
+
+# print config.strcfg
 
 application = web.application(urls, globals()).wsgifunc()
 
