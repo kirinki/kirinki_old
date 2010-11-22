42df8b2b485ad86679d8f62f34c6e60f3d41e8a2
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2010-11-22 14:23:37 +0100

    Se inicializan las variables de la clase de usuario y se definen los metodos para obtener y asignar las variables de la clase y los metodos (aun por implementar) de registro y acceso de usuarios

--- server/rstr_user.py
+++ server/rstr_user.py
@@ -5,15 +5,28 @@ from web import form
 class user:
     """User class"""
     def __init__(self):
+        self.data = {'username' : '', 'password' : '', 'name' : '', 'surname' : '', 'videos' : [] }
         register = form.Form(form.Textbox('username'),
                              form.Password('password'),
                              form.Password('password_again'),
-                             validators = [form.Validator("Las contrasenas no coinciden.", lambda i: i.password == i.password_again)])
-    #form.Validator("El nombre de usuario seleccionado ya existe", lambda i: !session['usr'].checkUsername(i.username))])
+                             validators = [form.Validator("Las contrasenas no coinciden.", lambda i: i.password == i.password_again),
+                                           form.Validator("El nombre de usuario seleccionado ya existe", lambda i: not session['usr'].checkUsername(i.username))])
     
         login = form.Form(form.Textbox('username'),
-                          form.Password('password'))
-                      #validators = [form.Validator("Usuario o contrasena incorrectos.", lambda i: session['usr'].login(i))])
+                          form.Password('password'),
+                          validators = [form.Validator("Usuario o contrasena incorrectos.", lambda i: session['usr'].login(i))])
 
         self.reg = register()
         self.log = login()
+
+    def checkUsername(self, user):
+        return True
+
+    def login(self, data):
+        return True
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
