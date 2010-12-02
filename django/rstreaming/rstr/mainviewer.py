4967e75ce255111158ea5d9e5b1ff250082f0ac7
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2010-12-02 14:28:08 +0100

    Se mejora el uso de las plantillas, se termina la vista del login y se crea una funcion jquery para igualar la altura de la segunda y la tercera columna, de mnera que la linea que divide las columnas sean igual de altas

--- django/rstreaming/rstr/mainviewer.py
+++ django/rstreaming/rstr/mainviewer.py
@@ -17,17 +17,22 @@ class MainViewer:
         if out == 'index' or out == 'logout' or out == 'about' or out == 'streaming' or out == 'videos' or out == 'stream' or out == 'upload' or out == 'admin':
             leftBlocks = []
             if not self.session_data['user'].is_authenticated():
-                leftBlocks = [render_to_string('rstr/login.html', {'session' : self.session_data}, context_instance=RequestContext(self.request))]
+                leftBlocks = [render_to_string('rstr/section.html', {'title' : 'login', 'content': render_to_string('rstr/login.html', {'session' : self.session_data}, context_instance=RequestContext(self.request))})]
+            centerBlocks = [render_to_string('rstr/section.html', {'title' : 'Bienvenido', 'content' : 'Bienvenido al sitio web de Ritho.'})]
+            rightBlocks = [self.generateArticles(), self.generateVideos()]
             self.page = render_to_response('rstr/index.html', {'copy' : '&copy; Pablo Alvarez de Sotomayor Posadillo',
                                                                'session' : self.session_data,
                                                                'leftCol' : self.getLeftCol(leftBlocks),
-                                                               'centerCol' : self.getCenterCol(),
-                                                               'rightCol' : self.getRightCol()}, context_instance=RequestContext(self.request))
+                                                               'centerCol' : self.getCenterCol(centerBlocks),
+                                                               'rightCol' : self.getRightCol(rightBlocks)}, context_instance=RequestContext(self.request))
         elif out == 'login':
+            centerBlocks = []
+            if not self.session_data['user'].is_authenticated():
+                centerBlocks = [render_to_string('rstr/section.html', {'title' : 'login', 'content': render_to_string('rstr/login.html', {'session' : self.session_data}, context_instance=RequestContext(self.request))})]
             self.page = render_to_response('rstr/index.html', {'copy' : '&copy; Pablo Alvarez de Sotomayor Posadillo',
                                                                'session' : self.session_data,
                                                                'leftCol' : self.getLeftCol(),
-                                                               'centerCol' : self.getCenterCol(),
+                                                               'centerCol' : self.getCenterCol(centerBlocks),
                                                                'rightCol' : self.getRightCol()}, context_instance=RequestContext(self.request))
         elif out == 'register':
             self.page = render_to_response('rstr/index.html', {'copy' : '&copy; Pablo Alvarez de Sotomayor Posadillo',
@@ -41,7 +46,15 @@ class MainViewer:
         return render_to_string('rstr/left.html', {'blocks' : blocks})
 
     def getCenterCol(self, blocks = []):
-        return render_to_string('rstr/center.html', {})
+        return render_to_string('rstr/center.html', {'blocks' : blocks})
 
     def getRightCol(self, blocks = []):
-        return render_to_string('rstr/right.html', {})
+        return render_to_string('rstr/right.html', {'blocks' : blocks})
+
+    def generateArticles(self):
+        article = render_to_string('rstr/article.html', {'title' : 'Inicio del proyecto', 'date' : '2010-12-01','abstract' : 'Con la inauguracion de esta oficial comienza su andadura el proyecto.', 'content' : 'Explicacion del proyecto RStreaming'})
+        return render_to_string('rstr/section.html', {'title' : 'Ultimas noticias', 'content' : article})
+
+    def generateVideos(self):
+        videos = render_to_string('rstr/video.html', {'width' : '320', 'height' : '240', 'controls' : True, 'src': 'file:///home/i02sopop/Downloads/PiTP - 2009 - Monday, July 13, 2009 - Kernighan.hi.mp4'})
+        return render_to_string('rstr/section.html', {'title' : 'Videos', 'content' : videos})
--- django/rstreaming/static/ritho.js
+++ django/rstreaming/static/ritho.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-    $('article').bind('keyup focusout',function(event){
-	alert($('article').html());
-    });
-
-    $('section').mousedown(function(event){
-	if(event.which == 1)
-	    $('section').bind('mousemove',$('section').move());
-    });
-
-    $('section').mouseup(function(event){	
-	if(event.which == 1)
-	    $('section').unbind('mousemove',$('section').move());
-    });
-
     $('ul.hmenu li.userMenu').mouseover(
 	function(event) {
 	    $('ul.hmenu li.userMenu ul').css("display","block");
@@ -24,8 +10,28 @@ $(document).ready(function() {
 	    $('ul.hmenu li ul').css("display","none");
 	}
     );
-});
 
-jQuery.fn.move = function() {
-    this.addClass('floating');
-};
+    $('div.col21').ready(
+	function(event) {
+	    if($('div.col21').height() < $('div.col22').height() ) {
+		$('div.col21').css("height", $('div.col22').height());
+	    }
+
+	    if($('div.col21').height() > $('div.col22').height() ) {
+		$('div.col22').css("height", $('div.col21').height());
+	    }
+	}
+    );
+
+    $('div.col22').ready(
+	function(event) {
+	    if($('div.col21').height() < $('div.col22').height() ) {
+		$('div.col21').css("height", $('div.col22').height());
+	    }
+
+	    if($('div.col21').height() > $('div.col22').height() ) {
+		$('div.col22').css("height", $('div.col21').height());
+	    }
+	}
+    );
+});
