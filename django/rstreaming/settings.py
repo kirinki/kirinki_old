c4f665bf3aff2d67661a414b1ef3899048030628
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2011-01-25 23:58:35 +0100

    Se hacen algunas configuraciones para probar el portal en el portatil

--- django/rstreaming/settings.py
+++ django/rstreaming/settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'kirinki',                      # Or path to database file if using sqlite3.
-        'USER': 'kuser',                      # Not used with sqlite3.
+        'USER': 'i02sopop',                      # Not used with sqlite3.
         'PASSWORD': 'passwd',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/usr/src/rstreaming/django/rstreaming/upload/'
+MEDIA_ROOT = '/home/i02sopop/desarrollo/rstreaming/django/rstreaming/upload/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -90,7 +90,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/usr/src/rstreaming/django/rstreaming/templates'
+    '/home/i02sopop/desarrollo/rstreaming/django/rstreaming/templates'
     # 'templates'
 )
 
