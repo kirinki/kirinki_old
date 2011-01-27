69d30cf1569e4f3247105ff8681b915731ca321c
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2011-01-27 15:34:02 +0100

    Cambio el flujo de branches, con lo que hago algunos reajustes ya hechos anteriormente

--- django/rstreaming/settings.py
+++ django/rstreaming/settings.py
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/i02sopop/desarrollo/rstreaming/django/rstreaming/upload/'
+MEDIA_ROOT = '/usr/src/rstreaming/django/rstreaming/upload/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -90,7 +90,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/i02sopop/desarrollo/rstreaming/django/rstreaming/templates'
+    '/usr/src/rstreaming/django/rstreaming/templates'
     # 'templates'
 )
 
