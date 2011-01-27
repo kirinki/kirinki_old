b29cd21ff786db52eb3fd94f6bb71784ea0af4dc
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2011-01-27 15:36:56 +0100

    Se termina de poner en algunos parametros valores por defecto

--- django/rstreaming/settings.py
+++ django/rstreaming/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'kirinki',                      # Or path to database file if using sqlite3.
-        'USER': 'i02sopop',                      # Not used with sqlite3.
-        'PASSWORD': 'passwd',                  # Not used with sqlite3.
+        'USER': 'dbuser',                      # Not used with sqlite3.
+        'PASSWORD': 'dbpasswd',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
