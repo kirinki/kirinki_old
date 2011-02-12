8e5ff3690f78827f58c8ca35a981ef718446687a
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2011-02-12 18:50:03 +0100

    Se pone solo la web de github en esta rama

--- apache/django.wsgi
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.append('/var/www/')
-sys.path.append('/var/www/kirinki')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'kirinki.settings'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
--- kirinki/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-__license__ = "GNU General Public License, Ver.3"
-__author__ = "Pablo Alvarez de Sotomayor Posadillo"
-
-from kirinki.models import *
-from django.contrib import admin
-
-admin.site.register(configs)
-admin.site.register(video)
